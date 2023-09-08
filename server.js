609c80ce76994622451fb88f9f3c7665f19ebe6f
sherwin25 <sherwinmotlagh@yahoo.com>
2023-09-08 20:18:33 +0800

    implement basic Crud Functionalities using Node.js and MongoDB

--- server.js
+++ server.js
@@ -1,11 +1,94 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const Product = require("./models/ProductModel");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.use(express.json());
+
+app.get("/products", async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json(products);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: error.message,
+    });
+  }
 });
 
-app.listen(3000, () => {
-  console.log("Server started on port 3000");
+app.get("/products/:id", async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    res.status(200).json(product);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: error.message,
+    });
+  }
 });
+
+app.put("/products/:id", async (req, res) => {
+  try {
+    const product = await Product.findByIdAndUpdate(req.params.id, req.body);
+    if (!product) {
+      return res.status(404).json({
+        message: "Product not found",
+      });
+    }
+    const updatedProduct = await Product.findById(req.params.id);
+    res.status(200).json(updatedProduct);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+});
+
+app.post("/products", async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.status(200).json(product);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+});
+
+app.delete("/products/:id", async (req, res) => {
+  try {
+    const product = await Product.findByIdAndDelete(req.params.id);
+    if (!product) {
+      return res.status(404).json({
+        message: "Product not found",
+      });
+    }
+    res.status(200).json({
+      message: "Product deleted successfully",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+});
+
+mongoose
+  .connect(
+    "mongodb+srv://admin:admin@restfulapi.9adhpta.mongodb.net/Node-Api?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("Connected to database");
+    app.listen(3000, () => {
+      console.log("Server started on port 3000");
+    });
+  })
+  .catch(() => {
+    console.log(error);
+  });
